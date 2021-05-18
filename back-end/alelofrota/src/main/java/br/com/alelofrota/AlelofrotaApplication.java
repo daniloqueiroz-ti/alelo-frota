1c04d0f9efdd183999f2a185954ac4651a856d51
danilo.queiroz <danilo.queiroz@crptecnologia.com.br>
2021-05-18 20:36:53 -0300

    created project backend

---
+++ back-end/alelofrota/src/main/java/br/com/alelofrota/AlelofrotaApplication.java
@@ -0,0 +1,13 @@
+package br.com.alelofrota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlelofrotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlelofrotaApplication.class, args);
+	}
+
+}
---
+++ back-end/alelofrota/src/test/java/br/com/alelofrota/AlelofrotaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alelofrota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlelofrotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
