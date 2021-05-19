2000c80ee191240f51c13f498e73d0cdfeeae4a2
danilo.queiroz <danilo.queiroz@crptecnologia.com.br>
2021-05-19 19:57:27 -0300

    added componet list and update to vehicle

--- front-end/alelofrota/src/app/app-routing.module.ts
+++ front-end/alelofrota/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { ReportsComponent } from './reports/reports.component';
+import { VehicleComponent } from './vehicle/vehicle.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: DashboardComponent
+  },
+  {
+    path: "vehicle",
+    component: VehicleComponent
+  },
+  {
+    path: "reports",
+    component: ReportsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end/alelofrota/src/app/app.module.ts
+++ front-end/alelofrota/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { VehicleComponent } from './vehicle/vehicle.component';
 import { ReportsComponent } from './reports/reports.component';
+import { ListVehicleComponent } from './vehicle/list-vehicle/list-vehicle.component';
+import { UpdateVehicleComponent } from './vehicle/update-vehicle/update-vehicle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     VehicleComponent,
-    ReportsComponent
+    ReportsComponent,
+    ListVehicleComponent,
+    UpdateVehicleComponent
   ],
   imports: [
     BrowserModule,
